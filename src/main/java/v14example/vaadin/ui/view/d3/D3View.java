83103f606ba1d2002a45197c0c9d03fe5c12e996
Mark <github@hissinkmuller.nl>
2019-08-22 20:23:27 +0200

    Upgrade to v4 tree

--- src/main/java/v14example/vaadin/ui/view/d3/D3View.java
+++ src/main/java/v14example/vaadin/ui/view/d3/D3View.java
@@ -2,12 +2,16 @@ package v14example.vaadin.ui.view.d3;
 
 import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.page.PendingJavaScriptResult;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.dom.DomEventListener;
 import com.vaadin.flow.function.SerializableConsumer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -24,107 +28,115 @@ import java.util.List;
 
 @Route(value = "", layout = MainLayout.class)
 @PageTitle("D3 View")
-// This is not working or needed, since we add the correct library below.
-// @JavaScript("https://d3js.org/d3.v5.min.js")
-// @JavaScript("http://d3js.org/d3.v3.min.js")
 public class D3View extends VerticalLayout
 {
     private static Log log = LogFactory.getLog(D3View.class);
 
+    private Page page = null;
+
+    private boolean alreaderRendered = false;
+
     public D3View()
     {
+        page = UI.getCurrent().getPage();
     }
 
     protected void onAttach(AttachEvent attachEvent)
     {
         super.onAttach(attachEvent);
 
-        Label anchor = new Label("Placeholder");
-        anchor.getElement().setProperty("innerHTML", "<anchor>");
-        add(anchor);
+        page = attachEvent.getUI().getPage();
+        // retrieves from /src/main/resources/META-INF/resources/frontend/js/
+        page.addStyleSheet("js/v4/style.css");
 
-        HorizontalLayout hl = new HorizontalLayout();
-        hl.setJustifyContentMode(JustifyContentMode.START);
+        page.addJavaScript("js/v4/d3.v4.js");
+        // page.addJavaScript("js/v4/d3.v4.min.js");
 
-        hl.add(new D3Anchor()); // Creates the <div> anchor, which is referred in tree.js:43.
-        add(hl);
+        page.addJavaScript("js/v4/script.js");
+        page.addJavaScript("js/v4/search.js"); // retrieves from /src/main/resources/META-INF/resources/frontend/js/
+        page.addJavaScript("js/v4/tree.js"); // retrieves from /src/main/resources/META-INF/resources/frontend/js/
+
+        addJsEventListener();
 
         addTree(attachEvent);
 
-        // addThreeBalls(attachEvent);
+        // add(SimpleD3Example.createThreeBallsExample(attachEvent));
     }
 
     private void addTree(AttachEvent attachEvent)
     {
-        setId("tree");
-        
+        this.setId("tree");
+
+        if (!alreaderRendered)
+        {
+            this.add(getTreeMenu());
+
+            page.executeJs("treeAlreadyRendered=false;renderTree();");
+            alreaderRendered = true;
+        }
+
         Page page = attachEvent.getUI().getPage();
 
-        page.addJavaScript("http://d3js.org/d3.v3.min.js");
-        page.addJavaScript("js/tree.js"); // retrieves from /src/main/resources/META-INF/resources/frontend/js/tree.js
-        page.executeJs("treeAlreadyRendered=false;renderTree();");
+        // page.addJavaScript("http://d3js.org/d3.v3.min.js");
+        // page.addJavaScript("js/tree.js"); // retrieves from /src/main/resources/META-INF/resources/frontend/js/tree.js
+        // page.executeJs("treeAlreadyRendered=false;renderTree();");
     }
 
-    @Tag("div")
-    public static class D3Anchor extends Component
+    private void addJsEventListener()
     {
-        public D3Anchor()
+        // click, mouseover, mouseout, mousemove
+        // load, change,
+        // See: https://www.w3schools.com/js/js_htmldom_events.asp
+
+        this.getElement().addEventListener("click", new DomEventListener()
         {
-            // Nothing is needed here, since just the <div> element is needed as anchor.
-            // Relevant documentation: https://vaadin.com/docs/v13/flow/creating-components/tutorial-component-container.html
-        }
+            @Override
+            public void handleEvent(com.vaadin.flow.dom.DomEvent event)
+            {
+                log.info("***** Click event received. The event is:");
+                log.info("phase: "+event.getPhase());
+                log.info("type: "+event.getType());
+                log.info("source: "+event.getSource());
+                log.info("data: "+event.getEventData());
+            }
+        });
+    }
+
+    private HorizontalLayout getTreeMenu()
+    {
+        HorizontalLayout treeMenu = new HorizontalLayout();
+        treeMenu.setWidthFull();
+        treeMenu.setAlignItems(Alignment.END);
+        treeMenu.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        Button expandButton = new Button("Expand all", e -> page.executeJs("myTree.expandTree()"));
+        Button collapseButton = new Button("Collapse all", e -> page.executeJs("myTree.collapseTree()"));
+
+        NumberField widthField = new NumberField("Width");
+
+        widthField.setPattern("[0-9.,]*");
+        widthField.setPreventInvalidInput(true);
+        widthField.setHasControls(true);
+        widthField.setValue((double) 90);
+        widthField.setStep(10);
+        widthField.setPattern("^[1-9][0-9]$");
+        widthField.addValueChangeListener(e -> page.executeJs("myTree.updateWidth("+widthField.getValue()+")"));
+
+        treeMenu.add(expandButton, collapseButton, widthField);
+
+        return treeMenu;
     }
 
 // -----------
-
-    @Tag("svg")
-    public static class ThreeBalls extends Component
-    {
-        public ThreeBalls()
-        {
-            getElement().setProperty("innerHTML",
-                    "<svg width=\"720\" height=\"120\">"
-                            + "  <circle cx=\"40\" cy=\"60\" r=\"10\"></circle>"
-                            + "  <circle cx=\"80\" cy=\"60\" r=\"10\"></circle>"
-                            + "  <circle cx=\"120\" cy=\"60\" r=\"10\"></circle>"
-                            + "</svg>");
-        }
-    }
-
-    private void addThreeBalls(AttachEvent attachEvent)
-    {
-        add(new ThreeBalls());
-
-        ComboBox<String> colorSelect = new ComboBox<>();
-        List<String> colors = new ArrayList<>();
-        colors.add("Fuchsia");
-        colors.add("Gold");
-        colors.add("Teal");
-        colors.add("Aqua");
-        add(colorSelect);
-        colorSelect.setItems(colors);
-        Page page = attachEvent.getUI().getPage();
-        colorSelect.addValueChangeListener(e ->
-        {
-            page.executeJs("d3.selectAll('circle').style('fill',$0)",
-                    colorSelect.getValue());
-        });
-    }
 
 // ----------------------
     private void showAlert()
     {
         Page page = UI.getCurrent().getPage();
 
-        page.addHtmlImport("html/htmlimport.html");
-
-        String d3js_location = "https://d3js.org/d3.v5.min.js";
-        page.addJavaScript(d3js_location);
-
         String alertExpression = "alert( 'Hello, world!' );";
 
         PendingJavaScriptResult pendingResult = page.executeJs(alertExpression);
-
         pendingResult.then(new SerializableConsumer<JsonValue>()
         {
             @Override
