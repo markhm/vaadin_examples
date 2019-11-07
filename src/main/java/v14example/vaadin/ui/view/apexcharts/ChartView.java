8f66cddbffb5047e20f1dfa48aff90e7d53df39d
Mark <github@hissinkmuller.nl>
2019-11-07 20:30:38 +0100

    Added ChartView update example

---
+++ src/main/java/v14example/vaadin/ui/view/apexcharts/ChartView.java
@@ -0,0 +1,57 @@
+package v14example.vaadin.ui.view.apexcharts;
+
+import com.github.appreciated.apexcharts.ApexCharts;
+import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.config.builder.ChartBuilder;
+import com.github.appreciated.apexcharts.config.builder.DataLabelsBuilder;
+import com.github.appreciated.apexcharts.config.builder.PlotOptionsBuilder;
+import com.github.appreciated.apexcharts.config.builder.XAxisBuilder;
+import com.github.appreciated.apexcharts.config.chart.Type;
+import com.github.appreciated.apexcharts.config.plotoptions.builder.BarBuilder;
+import com.github.appreciated.apexcharts.helper.Series;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import v14example.vaadin.ui.MainLayout;
+
+@PageTitle("Chart Update View")
+@Route(value = "", layout = MainLayout.class)
+public class ChartView extends VerticalLayout
+{
+    public ChartView()
+    {
+        ApexCharts chart = ApexChartsBuilder.get().withChart(ChartBuilder.get()
+                .withType(Type.bar)
+                .build())
+                .withPlotOptions(PlotOptionsBuilder.get()
+                        .withBar(BarBuilder.get()
+                                .withHorizontal(true)
+                                .build())
+                        .build())
+                .withDataLabels(DataLabelsBuilder.get()
+                        .withEnabled(false)
+                        .build())
+                .withSeries(new Series<>(400.0, 430.0, 448.0, 470.0, 540.0, 580.0, 690.0, 1100.0, 1200.0, 1380.0))
+                .withXaxis(XAxisBuilder.get()
+                        .withCategories()
+                        .build())
+                .build();
+        chart.setHeight("400px");
+
+        Button update = new Button("Update", buttonClickEvent -> {
+            chart.updateSeries(new Series<>(400.0, 430.0, 448.0, 470.0, 540.0, 580.0, 690.0, 1100.0, 1200.0, 500.0));
+
+            UI ui = UI.getCurrent();
+            ui.push();
+
+            Notification.show("The chart was updated!");
+        });
+        Button trigger = new Button("Trigger");
+        add(chart, update, trigger);
+    }
+
+
+}
